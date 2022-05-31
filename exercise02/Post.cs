ecd02156d373fd81a77039de65f38d53ed3b3a75
spaso <spaso@DESKTOP-FEK00DU>
2022-05-31 15:08:27 +0300

    Add project files.

---
+++ exercise02/Post.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise02
+{
+    internal class Post
+    {
+
+        private string _title;
+        private string _description;
+        private int _votesCount;
+        
+
+        public DateTime Date { get; set; }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public int UpVote()
+        {
+            _votesCount++;
+            return _votesCount;
+        }
+
+        public int DownVote()
+        {
+            _votesCount--;
+            return _votesCount;
+        }
+
+        public int TotalVotes()
+        {
+            return _votesCount;
+        }
+
+    }
+}
---
+++ exercise02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace exercise02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var post = new Post();
+            
+
+            while (true)
+            {
+                Console.WriteLine("Type 'up-vote' or 'down-vote' to rate this post");
+                var input = Console.ReadLine();
+
+                if (input == "up-vote")
+                {
+                    post.UpVote();
+                }
+                else if (input == "down-vote")
+                {
+                    post.DownVote();
+                }
+                else if (string.IsNullOrWhiteSpace(input) || input == "quit")
+                {
+                    break;
+                }
+                
+            }
+            Console.WriteLine(post.TotalVotes());
+            
+        }
+    }
+}
+
